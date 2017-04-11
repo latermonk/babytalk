a1693baae127cfe21fdecb775dc0dfc575fbbcf4
latermonk <@>
2017-04-11 18:07:53 +0800

    Initial Commit

---
+++ babytalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  babytalk
+//
+//  Created by wei on 11/04/2017.
+//  Copyright © 2017 wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ babytalk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  babytalk
+//
+//  Created by wei on 11/04/2017.
+//  Copyright © 2017 wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
